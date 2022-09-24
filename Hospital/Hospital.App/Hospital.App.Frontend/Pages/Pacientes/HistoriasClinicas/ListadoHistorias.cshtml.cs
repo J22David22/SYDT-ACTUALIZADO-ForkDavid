15626f516c9607bdaa81d0dc19a7d1c67a306fd3
CarlosAndresMarin <cmarininstrumentista@gmail.com>
2022-09-23 20:47:30 -0500

    Creacion ListadoHistorias

---
+++ Hospital/Hospital.App/Hospital.App.Frontend/Pages/Pacientes/HistoriasClinicas/ListadoHistorias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hospital.App.Frontend.Pages
+{
+    public class ListadoHistoriasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
