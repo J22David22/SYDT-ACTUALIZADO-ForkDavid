7e3d9b83f3ab06576c16ff4032d186155b9281d5
CarlosAndresMarin <cmarininstrumentista@gmail.com>
2022-09-25 08:14:01 -0500

    Creacion Repo SugereCuidado

---
+++ Hospital/Hospital.App/Hospital.App.Frontend/Pages/SugerenciasCuidado/SugerenciasCuidado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hospital.App.Frontend.Pages
+{
+    public class SugerenciasCuidadoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
